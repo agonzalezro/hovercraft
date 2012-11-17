1e90faaf6afdf296679b59f2fb1510cc1c347175
Oscar Vilaplana <dev@oscarvilaplana.cat>
2012-11-17 15:00:12 +0100

    storage

--- hovercraft/storage.py
+++ hovercraft/storage.py
@@ -1,11 +1,14 @@
 import redis
 class Storage(object):
-    storage = redis.StrictRedis(host='localhost', port=6379, db=0)
+    _backend = redis.StrictRedis(host='localhost', port=6379, db=0)
 
     def get(self, key):
-        return self.storage.get(key)
+        return self._backend.get(key)
 
     def set(self, key, value):
-        return self.storage.set(key, value)
+        return self._backend.set(key, value)
 
+    def delete(self, key):
+        return self._backend.delete(key)
+    
 storage = Storage()
