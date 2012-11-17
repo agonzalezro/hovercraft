57bcbef44bce6c93b860ddd9cdce23e311b0fddb
Oscar Vilaplana <dev@oscarvilaplana.cat>
2012-11-17 12:35:03 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='hovercraft',
+      version='0.1',
+      description='Hovercraft Slides',
+      author='Pygrunn',
+      install_requires=[
+        'flask',
+        'flask_oauth',
+        ],
+     )
